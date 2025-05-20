0d8d050fa48584f2f0e926cc3b6a596491ed7bfa
Usman <muhammad.uthman@palmpy-inc.com>
2025-05-20 17:32:01 +0100

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,17 @@ module.exports = {
     extend: {
       colors: {
         primary: "#030014",
+        secondary: "#151312",
+        accent: "#ab8bff",
+        light: {
+          100: "#d6c6ff",
+          200: "#a8bfdb",
+          300: "#pca4ab",
+        },
+        dark: {
+          100: "#221f3d",
+          200: "#0f0d23",
+        },
       },
     },
   },
