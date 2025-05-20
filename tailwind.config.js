e641767def9396c9c1ef9a424ed340d79ea238db
Usman <muhammad.uthman@palmpy-inc.com>
2025-05-20 11:20:29 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: [
+    "./App.tsx",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#030014",
+      },
+    },
+  },
+  plugins: [],
+};
